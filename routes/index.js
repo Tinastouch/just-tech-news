03c672a72dbd8a5594d57f11e14aa89a7e860ce4
Tinastouch <tinastouch@gmail.com>
2022-02-15 11:58:58 -0500

    backend complete

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,14 @@ router.use((req, res) => {
   res.status(404).end();
 });
 
+module.exports = router;const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
 module.exports = router;
